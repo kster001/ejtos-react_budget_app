13ebdd9aeaac6be077ba20daaf3b45c5e0f1d056
Terry Ter <TerryTer@primustech.com.sg>
2023-07-05 17:58:50 +0800

    new changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    const setBudget = (newBudget) => {
+        if (newBudget > 20000) {
+            alert(`The budget (£${newBudget}) cannot exceed the limit of £20000`);
+            return;
+        } 
+        console.log(newBudget, expenses);
+        if(newBudget < totalExpenses) {
+            alert(`You cannot reduce the budget value lower than the spending`);
+            return;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget
+        });
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                value={budget}
+                step={10}
+                style={{ marginLeft: '2rem' , size: 10}}
+                onChange={(event) => setBudget(event.target.value)}
+                onKeyDown={(event) => setBudget(event.target.value)}>
+            </input>
         </div>
     );
 };
